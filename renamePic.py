d980011fa7841deee50f1d61c1911be2a28afbca
poemdistance <poemdistance@gmail.com>
2019-04-30 13:41:50 +0800

    fix bug

--- renamePic.py
+++ renamePic.py
@@ -1,4 +1,5 @@
 import os
+import stat
 import sys
 
 length = len(sys.argv)
@@ -12,20 +13,44 @@ else:
         i = int(f.read())
         f.close()
 
+        dot = ''
+        name = sys.argv[len(sys.argv)-1].split('.')
+        for s in name:
+            if s == '':
+                dot += '.'
+
     for n in range(1, length):
         files = os.listdir(sys.argv[n])
         for f in files:
             f = os.path.join(sys.argv[n], f)
             name = f.split('.')
-            if name[1] != 'py':
-                os.rename(f, name[0]+'tmp'+'.'+name[1])
+            if name[len(name)-1] == 'mp4' \
+                    or name[len(name)-1] == 'jpg' \
+                    or name[len(name)-1] == 'jpeg' \
+                    or name[len(name)-1] == 'png' \
+            :
+                dst = name[len(name)-2]+'tmp'+'.'+name[len(name)-1]
+
+                dst = dot + dst
+
+                #先文件名后面添加tmp生成临时文件名
+                #防止可能会覆盖掉相同的文件名
+                os.rename(f, dst)
 
         files = os.listdir(sys.argv[n])
         for f in files:
             f = os.path.join(sys.argv[n], f)
             name = f.split('.')
-            if name[1] != 'py':
-                os.rename(f, os.path.join(sys.argv[n], str(i)+'.'+name[1]))
+            if name[len(name)-1] == 'mp4' \
+                    or name[len(name)-1] == 'jpg' \
+                    or name[len(name)-1] == 'jpeg' \
+                    or name[len(name)-1] == 'png' \
+            :
+                dst = str(i) + '.' + name[len(name)-1]
+                dst = os.path.join( sys.argv[n], dst )
+
+                #以序号重命名文件
+                os.rename(f,dst)
                 i += 1
 
     with open('endNum', 'w') as f:
